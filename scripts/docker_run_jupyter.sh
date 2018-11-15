79a0f2aefaa5dbaf6ee2def04fcfc3b36e757152
Eunsoo Sheen <eunsoo.sheen@gmail.com>
2018-11-15 11:18:11 -0800

    fix scripts to see docker command

--- scripts/docker_run_jupyter.sh
+++ scripts/docker_run_jupyter.sh
@@ -28,6 +28,15 @@ if [ -n "$CHECK_CONTAINER" ]; then
   echo "$JUPYTER_NAME is running, stopping and deleting"
   docker rm -f $JUPYTER_NAME
 fi
+
+echo "docker command: "
+echo "$DOCKER run -i -t -d --name $JUPYTER_NAME \
+--privileged --restart=always \
+-p $JUPYTER_PORT:8888 -p $JUPYTER_RESTAPIPORT:8088 \
+-v $JUPYTER_VOLUME:/root/volume \
+-e JUPYTER_BASEURL=$JUPYTER_BASEURL -e JUPYTER_PASSWORD=$JUPYTER_PASSWORD \
+$IMAGE:$TAG /root/volume/scripts/run_jupyter.sh"
+
 $DOCKER run -i -t -d --name $JUPYTER_NAME \
 --privileged --restart=always \
 -p $JUPYTER_PORT:8888 -p $JUPYTER_RESTAPIPORT:8088 \
