a0b3b73b4d9680e3a71dac50c604a4344afbe337
Eunsoo Sheen <eunsoo.sheen@gmail.com>
2018-11-15 11:51:50 -0800

    fix bug

---
+++ scripts/docker_restart_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SOURCE_DIR=`dirname ${BASH_SOURCE[0]}`
+cd $SOURCE_DIR
+source ./env.sh
+cd ..
+
+if [ "$JUPYTER_GPU" = "TRUE" ]
+then
+DOCKER="nvidia-docker"
+IMAGE="$JUPYTER_GPU_IMAGE"
+TAG="$JUPYTER_GPU_TAG"
+else
+DOCKER="docker"
+IMAGE="$JUPYTER_IMAGE"
+TAG="$JUPYTER_TAG"
+fi
+
+$DOCKER start $JUPYTER_NAME
