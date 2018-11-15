2512547d90fd689f658431c5fb6dd51f5e286482
Eunsoo Sheen <eunsoo.sheen@gmail.com>
2018-11-15 11:49:18 -0800

    fix bug

--- scripts/run_jupyter.sh
+++ scripts/run_jupyter.sh
@@ -4,13 +4,16 @@ SPARKENV_PATH="/root/volume/scripts/spark-env.sh"
 REQUIREMENTS_PATH="/root/volume/docker/requirements.txt"
 JUPYTERLOG_PATH="/root/volume/logs/jupyter.log"
 
+mkdir -p /root/volume/logs
+
 #Spark Env
+echo "setting spark environment" >$JUPYTERLOG_PATH
 cp $SPARKENV_PATH /usr/local/spark/conf/spark-env.sh
 
+echo "install python modules in requirement.txt" >$JUPYTERLOG_PATH
 pip2 install -r $REQUIREMENTS_PATH >> $JUPYTERLOG_PATH 2>&1
 pip3 install -r $REQUIREMENTS_PATH >> $JUPYTERLOG_PATH 2>&1
 
-mkdir -p /root/volume/logs
 mkdir /root/.jupyter >> $JUPYTERLOG_PATH 2>&1
 cp /root/volume/docker/jupyter_notebook_config.py /root/.jupyter/ >> $JUPYTERLOG_PATH 2>&1
 jupyter contrib nbextension install --user >> $JUPYTERLOG_PATH 2>&1
